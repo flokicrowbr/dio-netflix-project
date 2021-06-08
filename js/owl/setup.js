a0f37d2305df4b750f0c886dbf87604f92e70545
flokicrowbr <jc_meissner@hotmail.com>
2021-06-08 17:43:16 -0300

    Atualização do projeto e responsividade

---
+++ js/owl/setup.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:7,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
